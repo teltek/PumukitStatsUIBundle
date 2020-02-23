eaf10ab831990d6a5a20873bc11c7b8c25aed579
Pablo Ogando Ferreira <pabloogando5@gmail.com>
2020-02-23 21:20:59 +0100

    Upgrade bundle to make compatible with symfony 4

--- DependencyInjection/PumukitStatsUIExtension.php
+++ DependencyInjection/PumukitStatsUIExtension.php
@@ -5,20 +5,12 @@ namespace Pumukit\StatsUIBundle\DependencyInjection;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
-/**
- * This is the class that loads and manages your bundle configuration.
- *
- * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
- */
 class PumukitStatsUIExtension extends Extension
 {
-    /**
-     * {@inheritdoc}
-     */
-    public function load(array $configs, ContainerBuilder $container)
+    public function load(array $configs, ContainerBuilder $container): void
     {
         $configuration = new Configuration();
-        $config = $this->processConfiguration($configuration, $configs);
+        $this->processConfiguration($configuration, $configs);
 
         $permissions = [['role' => 'ROLE_ACCESS_STATS', 'description' => 'Access Stats']];
         $newPermissions = array_merge($container->getParameter('pumukitschema.external_permissions'), $permissions);
