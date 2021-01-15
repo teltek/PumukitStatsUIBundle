c4929ea62b90e0547c802d51a296f53116f1d0ab
Pablo Ogando Ferreira <pabloogando5@gmail.com>
2021-01-15 08:26:52 +0100

    Use renderView instead template annotation

--- Controller/StatsController.php
+++ Controller/StatsController.php
@@ -4,7 +4,6 @@ namespace Pumukit\StatsUIBundle\Controller;
 
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
@@ -19,22 +18,24 @@ class StatsController extends AbstractController
      * @Route("/objects", name="pumukit_stats_mmobj_index")
      * @Route("/series/{id}", name="pumukit_stats_series_index_id")
      * @Route("/objects/{id}", name="pumukit_stats_mmobj_index_id")
-     * @Template("@PumukitStatsUI/Stats/index.html.twig")
      */
     public function indexAction(Request $request)
     {
         $routeName = $request->get('_route');
         $translator = $this->get('translator');
 
-        if($routeName == 'pumukit_stats_series_index') {
+        if ($routeName == 'pumukit_stats_series_index') {
             $title = 'General Statistics for Series';
-        } elseif($routeName == 'pumukit_stats_mmobj_index') {
+        } elseif ($routeName == 'pumukit_stats_mmobj_index') {
             $title = 'General Statistics for Objects (videos/audios)';
-        } elseif($routeName == 'pumukit_stats_series_index_id') {
+        } elseif ($routeName == 'pumukit_stats_series_index_id') {
             $title = 'Statistics of the series';
         } else {
             $title = 'Statistics of the object';
         }
-        return array('template_title' => $translator->trans($title));
+
+        return $this->render("@PumukitStatsUI/Stats/index.html.twig", [
+            'template_title' => $translator->trans($title)
+        ]);
     }
 }
