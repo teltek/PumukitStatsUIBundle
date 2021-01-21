b81b46809862eb7278f63fecdb38e9f2ac62dde5
Pablo Ogando Ferreira <pabloogando5@gmail.com>
2021-01-21 10:11:51 +0100

    TTK-23461 fix stats paginator

--- Controller/StatsController.php
+++ Controller/StatsController.php
@@ -1,11 +1,14 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Pumukit\StatsUIBundle\Controller;
 
-use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 /**
  * @Route("/admin/stats")
@@ -19,23 +22,23 @@ class StatsController extends AbstractController
      * @Route("/series/{id}", name="pumukit_stats_series_index_id")
      * @Route("/objects/{id}", name="pumukit_stats_mmobj_index_id")
      */
-    public function indexAction(Request $request)
+    public function indexAction(Request $request): Response
     {
         $routeName = $request->get('_route');
         $translator = $this->get('translator');
 
-        if ($routeName == 'pumukit_stats_series_index') {
+        if ('pumukit_stats_series_index' === $routeName) {
             $title = 'General Statistics for Series';
-        } elseif ($routeName == 'pumukit_stats_mmobj_index') {
+        } elseif ('pumukit_stats_mmobj_index' === $routeName) {
             $title = 'General Statistics for Objects (videos/audios)';
-        } elseif ($routeName == 'pumukit_stats_series_index_id') {
+        } elseif ('pumukit_stats_series_index_id' === $routeName) {
             $title = 'Statistics of the series';
         } else {
             $title = 'Statistics of the object';
         }
 
-        return $this->render("@PumukitStatsUI/Stats/index.html.twig", [
-            'template_title' => $translator->trans($title)
+        return $this->render('@PumukitStatsUI/Stats/index.html.twig', [
+            'template_title' => $translator->trans($title),
         ]);
     }
 }
--- PumukitStatsUIBundle.php
+++ PumukitStatsUIBundle.php
@@ -1,10 +1,12 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Pumukit\StatsUIBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class PumukitStatsUIBundle extends Bundle
 {
-    const VERSION = '2.0.x-dev';
+    public const VERSION = '2.0.x-dev';
 }
